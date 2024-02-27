3e9e7cbe51eb7afa405ed6c7f946fd32aec15b11
codewithV <151612784+vaishnavikoli885@users.noreply.github.com>
2024-02-27 14:18:14 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -6,7 +6,7 @@ const listingSchema = new Schema({
         type: String,
         required: true,
     },
-    description: String,
+    description:String,
     image:  {
         type: String,
         default:"https://www.istockphoto.com/photo/palm-trees-silhouette-on-sunset-gm179117046-25310579?utm_campaign=srp_photos_bottom&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fsunset-with-cocount-tree&utm_medium=affiliate&utm_source=unsplash&utm_term=sunset+with+cocount+tree%3A%3A%3A",
@@ -14,9 +14,10 @@ const listingSchema = new Schema({
          ? "https://www.istockphoto.com/photo/palm-trees-silhouette-on-sunset-gm179117046-25310579?utm_campaign=srp_photos_bottom&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fsunset-with-cocount-tree&utm_medium=affiliate&utm_source=unsplash&utm_term=sunset+with+cocount+tree%3A%3A%3A"
          : v,
     },
-    price: Number,
+    price:Number,
     location: String,
     country: String,
+   
 });
 
 const Listing = mongoose.model("Listing" , listingSchema);
