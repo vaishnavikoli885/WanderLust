90e9fb9bfa442c119801d273bc901a7f08e7c561
codewithV <151612784+vaishnavikoli885@users.noreply.github.com>
2024-03-05 15:36:48 +0530

    Update review.js

--- models/review.js
+++ models/review.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const reviewSchema= new Schema({
-    Comment:String,
+    comment:String,
     rating:{
         type: Number,
         min: 1,
