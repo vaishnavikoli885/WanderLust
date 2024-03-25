9f283d8231a9038cf6ee7d956d8f999308c211de
codewithV <151612784+vaishnavikoli885@users.noreply.github.com>
2024-03-25 17:31:42 +0530

    Add files via upload

---
+++ routes/listing.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync= require("../utils/wrapAsync.js");
+const ExpressError= require("../utils/ExpressError.js");
+const { listingSchema } = require("../schema.js");
+const Listing = require("../models/listing.js");
+const {isLoggedIn} = require("../middleware.js");
+
+const validateListing = (req ,res, next)=>{
+  let {error} = listingSchema.validate(req.body);
+   if(error){
+     let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400 , errMsg);
+    }else{
+      next();
+    }
+};
+
+//Index Route
+router.get("/", 
+    wrapAsync(async (req, res) => {
+        const allListings = await Listing.find({});
+        res.render("listings/index.ejs", { allListings });
+    })
+);
+
+//New Route
+router.get("/new", isLoggedIn,(req, res) => {
+    res.render("listings/new.ejs");
+});
+
+//show route
+router.get("/:id",
+    wrapAsync(async (req,res) => {
+        let {id} = req.params;
+        const listing = await Listing.findById(id).populate("reviews").populate("owner");
+        if(!listing){
+            req.flash("error", "Listing you requested for does not exits!");
+            res.redirect("/listings");
+        }
+        console.log(listing);
+        res.render("listings/show.ejs", { listing });
+    })
+);
+
+//Create Route
+router.post("/", isLoggedIn,validateListing, 
+    wrapAsync(async (req , res, next) => {
+        const newListing = new Listing(req.body.listing);
+        newListing.owner = req.user._id;
+        await newListing.save();
+        req.flash("success", "New Listing Created!");
+        res.redirect("/listings");
+    })
+);
+
+// Edit Route
+router.get("/:id/edit",isLoggedIn,
+    wrapAsync(async (req, res) => {
+        let {id} = req.params;
+        const listing = await Listing.findById(id);
+        if(!listing){
+            req.flash("error", "Listing you requested for does not exits!");
+            res.redirect("/listings");
+        }
+        res.render("listings/edit.ejs", { listing }); 
+    })
+);
+
+//Update ROute
+router.put("/:id",isLoggedIn,validateListing,
+    wrapAsync(async(req, res) =>{
+        let {id} = req.params;
+        await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+        req.flash("success", " Listing Updated!");
+        res.redirect(`/listings/${id}`);
+    })
+);
+
+//Delete ROute
+router.delete("/:id", isLoggedIn,wrapAsync(async (req, res) =>{
+        let {id} = req.params;
+        let deletedListing = await Listing.findByIdAndDelete(id);
+        console.log(deletedListing);
+        req.flash("success", "Listing Deleted!");
+        res.redirect("/listings");
+    })
+);
+
+module.exports = router;
